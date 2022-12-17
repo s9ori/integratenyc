3b93c8b424bde5d7cc12417f117b7617b67a5441
s9ori <sebauchida@gmail.com>
2022-12-17 13:45:21 -0500

    sun icon 2

--- wp-content/themes/go/dist/js/school.js
+++ wp-content/themes/go/dist/js/school.js
@@ -10,8 +10,8 @@ toggleButton.addEventListener('click', () => {
   school_name.classList.toggle('dark');
   
     // Change the SVG icon on the button
-    if (icon.innerHTML.includes('M22.5')) {
-      // If the current icon is the sun icon, replace it with the moon icon
+    if (document.body.classList.contains('light')) {
+      // If the body element has the 'light' class, replace the icon with the moon icon
       icon.innerHTML = '<path d="M15,24a12.021,12.021,0,0,1-8.914-3.966,11.9,11.9,0,0,1-3.02-9.309A12.122,12.122,0,0,1,13.085.152a13.061,13.061,0,0,1,5.031.205,2.5,2.5,0,0,1,1.108,4.226c-4.56,4.166-4.164,10.644.807,14.41a2.5,2.5,0,0,1-.7,4.32A13.894,13.894,0,0,1,15,24Z"/>';
     } else {
       // If the current icon is not the sun icon, assume it is the moon icon and replace it with the sun icon
